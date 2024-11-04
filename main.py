e1492e99c7494e6cf9bab152e17cd4ca24a27bc7
Haider Imtiaz <haider.imtiaz38@ee.ceme.edu.pk>
2024-11-04 16:47:16 +0500

    Frozen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
@@ -10,6 +12,5 @@ def index():
 def left_sidebar():
     return render_template('Card.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.freeze()
